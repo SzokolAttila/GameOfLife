5767581601e0c83bd3c12f6b72a6da834bbde7df
SzokolAttila <szokol.ati2005@gmail.com>
2023-09-25 17:24:19 +0200

    interfaces

---
+++ GameOfLife/GameOfLife/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    internal interface IEntity
+    {
+        int[] Position { get; set; }
+        void EndOfTurn();
+        char Display { get; set; }
+    }
+}
